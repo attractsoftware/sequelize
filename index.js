9306d50df25b13a41c42b64402be2af2689d2719
Ravindran <attractsoftware.com>
2022-08-30 19:14:53 +0530

    first

---
+++ index.js
@@ -0,0 +1,65 @@
+const Sequelize=require("sequelize");  
+const sequelize=new Sequelize("db1",'postgres','asdfgf',{
+    host:'localhost',
+    dialect:"postgres",
+    define:{
+        freezeTableName:true
+        }
+},
+);
+
+sequelize.authenticate().then(()=>{
+    console.log("Connection Success");
+}).catch(()=>{
+    console.log(("error"));
+});
+
+// async function myFunction(){
+//     await sequelize.authenticate();
+//     console.log("connection Success");
+// }
+
+
+// myFunction();
+
+const User=sequelize.define('user',{
+    user_id:{
+       type:Sequelize.DataTypes.INTEGER ,
+       primaryKey:true,
+       autoIncrement:true
+    },
+    username:{
+        type:Sequelize.DataTypes.STRING,
+         allowNull:false,
+    },
+    email:{
+        type:Sequelize.DataTypes.STRING,
+         allowNull:true,
+    },
+    password:{
+        type:Sequelize.DataTypes.STRING
+        
+    },
+    age:{
+        type:Sequelize.DataTypes.INTEGER,
+        defaultValue:21
+    }
+},{
+   // freezeTableName:true,
+    timestamps:false,
+    initialAutoIncrement: 1000,
+    } );
+
+    User.drop();
+//User.sync({force:true}).then((data)=>{
+User.sync({alter:true}).then((data)=>{
+    console.log("Syancoronized successfull");
+    console.log(data);
+}).catch((err)=>{
+    console.log(err);
+})
+;
+
+//sequelize.drop();
+//sequelize.drop({match:/_test$/});
+console.log("Another");
